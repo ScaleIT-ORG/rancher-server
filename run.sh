881b25ba42cfea4bc38f89ac2abfa27739e7e189
olegkolner <oleg.kolner@gmail.com>
2017-12-20 15:52:12 +0200

    changed run.sh: was integrated usage of env variable

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+source .env
+
 docker-compose up -d
-docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.6 http://staging.teco.edu:8080/v1/scripts/AD71019242C53EBC7D69:1483142400000:Fab8RSflQbbbSopx6ixaiMlSs3E
+docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.6 http://staging.teco.edu:8080/v1/scripts/$TOKEN
 
 
 
